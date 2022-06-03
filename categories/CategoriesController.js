75204a9b51e59247194cd028293961005f2ed7a1
alexdemenezes <alexmenezes.business@outlook.com>
2022-06-02 21:21:15 -0300

    criacao do controller das categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const Category = require('./Category');
+const slugify = require('slugify');
 
 router.get('/categories', (req, res) => {
   return res.send('categioria');
 });
 
 router.get('/admin/categories/new', (req, res) => {
-  res.send('rota para criar um novo artigo');
-})
+  return res.render('admin/categories/new');
+});
+
+router.post('/categories/save', async (req, res) => {
+  const {
+    title
+  } = req.body;
+  console.log(title);
+
+  if (title) {
+    await Category.create({
+  title,
+  slug: slugify(title)
+});
+
+    return res.redirect('/');
+  }
+  return res.redirect("/admin/categories/new");
+});
+
+
+
 
 module.exports = router;
