e470fdbb5c09b40d3453ff6896f4ddd3a8523789
alexdemenezes <alexmenezes.business@outlook.com>
2022-06-03 10:27:15 -0300

    rota para a criacao de artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const Category = require('../categories/Category');
 
 router.get('/articles', (req, res) => {
   res.send('rota de artigos');
 });
 
-router.get('/admin/articles/new', (req, res) => {
-  res.send('rotas para criar artigos')
+router.get('/admin/articles/new', async (req, res) => {
+  try {
+    const categories = await Category.findAll();
+
+    return res.render('admin/articles/new', { categories });
+
+  } catch (e) {
+
+  }
 });
 
 module.exports = router;
