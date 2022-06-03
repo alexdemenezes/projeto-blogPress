6884aac56390392e70fb7e100b22e45c6a54c55c
alexdemenezes <alexmenezes.business@outlook.com>
2022-06-03 10:52:03 -0300

    configura connection do sequelize para ter o timezone do Brasil

--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,8 @@ const Sequelize = require('sequelize')
 
 const connection = new Sequelize('BLOG_PRESS', 'root', 'seattle6565301', {
   host: 'localhost',
-  dialect: 'mysql'
+  dialect: 'mysql',
+  timezone: '-03:00'
 });
 
 module.exports = connection;
